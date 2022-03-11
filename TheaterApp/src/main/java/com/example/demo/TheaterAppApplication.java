2c41a103378bf4e10ae218549f3e84ac0f603af4
tjrwns1202 <92131363+tjrwns1202@users.noreply.github.com>
2022-03-11 11:04:05 +0900

    test2323

---
+++ TheaterApp/src/main/java/com/example/demo/TheaterAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheaterAppApplication.class, args);
+	}
+
+}
