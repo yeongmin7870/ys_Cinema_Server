641bdfd5e9391dbfde8cca3487010561f23ab841
Drigter1999 <yah9600@gmail.com>
2022-03-12 17:04:18 +0900

    성공했나용???

---
+++ TheaterApp/src/test/java/com/vam/persistance/JDBCTest.java
@@ -0,0 +1,29 @@
+package com.vam.persistance;
+import java.sql.*;
+import org.junit.jupiter.api.Test;
+
+public class JDBCTest {
+	
+	@Test
+	public void testConnection() {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            String url = "jdbc:oracle:thin:@DB202203091416_high?TNS_ADMIN=./src/main/resources/Wallet/";
+            String id = "admin";
+            String pwd = "Cloudoracle123";
+
+            Class.forName("oracle.jdbc.OracleDriver");
+            conn = DriverManager.getConnection(url,id,pwd);
+
+            String sql="insert into user1 (id,name) values(11,'1')";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
