65ee42d3a439aa472e15f2cc3a9c4885ae609fa8
ymgood <minala7870@gmail.com>
2022-03-12 22:39:22 +0900

    practice

--- TheaterApp/src/main/java/com/example/demo/TheaterAppApplication.java
+++ TheaterApp/src/main/java/com/example/demo/TheaterAppApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TheaterAppApplication {
@@ -10,4 +11,8 @@ public class TheaterAppApplication {
 		SpringApplication.run(TheaterAppApplication.class, args);
 	}
 
+	@GetMapping
+	public String helloWorld(){
+		return "I wanna go Mars";
+	}
 }
