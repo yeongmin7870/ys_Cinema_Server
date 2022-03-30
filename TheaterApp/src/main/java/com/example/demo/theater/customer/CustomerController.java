e77cee7e7eb64bf486f3c0af71ccdaa54d751f4f
ymgood <minala7870@gmail.com>
2022-03-30 20:39:27 +0900

    spring security

--- TheaterApp/src/main/java/com/example/demo/theater/customer/CustomerController.java
+++ TheaterApp/src/main/java/com/example/demo/theater/customer/CustomerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/theater")
 public class CustomerController {
 
     @Autowired
