4f110f666c29f545c603c1258673c7f5897ba5cb
nanowl <hanjy20129@gmail.com>
2022-05-15 19:04:12 +0900

    add a customer html folder & Implement a custInsert.html page

---
+++ TheaterApp/src/main/java/com/example/demo/theater/controller/CutomerController.java
@@ -0,0 +1,17 @@
+package com.example.demo.theater.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CutomerController {
+
+    @GetMapping("/custInsert")
+    public String insert() {
+        return "/customer/custInsert";
+    }
+
+}
