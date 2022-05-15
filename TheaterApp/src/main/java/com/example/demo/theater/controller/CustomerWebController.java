3eaa1b7f068450eb4ba6f8c2fd59fb859a18a712
nanowl <hanjy20129@gmail.com>
2022-05-15 19:15:29 +0900

    naming update

--- TheaterApp/src/main/java/com/example/demo/theater/controller/CustomerWebController.java
+++ TheaterApp/src/main/java/com/example/demo/theater/controller/CustomerWebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
-public class CutomerController {
+public class CustomerWebController {
 
     @GetMapping("/custInsert")
     public String insert() {
