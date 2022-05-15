15f2e9ee911c1d6a2e8c7597fb14dd79e1d3b413
Drigter1999 <yah9600@gmail.com>
2022-05-15 22:56:46 +0900

    이상기

--- TheaterApp/src/main/java/com/example/demo/theater/normaltheaterseat/NormalTheaterSeatRepository.java
+++ TheaterApp/src/main/java/com/example/demo/theater/normaltheaterseat/NormalTheaterSeatRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NormalTheaterSeatRepository extends JpaRepository<NormalTheaterSeat,Integer> {
+public interface NormalTheaterSeatRepository extends JpaRepository<NormalTheaterSeat,String> {
 }
--- TheaterApp/src/main/java/com/example/demo/theater/specialTheaterSeat/SpecialTheaterSeatRepository.java
+++ TheaterApp/src/main/java/com/example/demo/theater/specialTheaterSeat/SpecialTheaterSeatRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.theater.specialTheaterSeat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SpecialTheaterSeatRepository extends JpaRepository {
+@Repository
+public interface SpecialTheaterSeatRepository extends JpaRepository<SpecialTheaterSeat, String> {
 }
