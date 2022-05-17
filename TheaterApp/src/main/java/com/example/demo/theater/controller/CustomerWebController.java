e83fe5383e06361e6d7a72cf9d4b71a11110abcd
nanowl <hanjy20129@gmail.com>
2022-05-17 11:50:26 +0900

    customerWebPage UI design complete.

--- TheaterApp/src/main/java/com/example/demo/theater/controller/CustomerWebController.java
+++ TheaterApp/src/main/java/com/example/demo/theater/controller/CustomerWebController.java
@@ -1,18 +1,33 @@
 package com.example.demo.theater.controller;
 
 
+import com.example.demo.theater.customer.Customer;
+import com.example.demo.theater.customer.CustomerDaoService;
+import com.example.demo.theater.customer.CustomerNotFoundException;
+import com.example.demo.theater.customer.CustomerRepository;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerWebController {
 
+    @Autowired
+    private CustomerDaoService service;
+
     @GetMapping("/custInsert")
     @ApiOperation("회원정보등록 페이지")
-    public String insert() {
+    public String insert(@RequestBody Customer newCustomer) {
+        Customer customer = service.findById(newCustomer.getCustomerId());
+
         return "/customer/custInsert";
     }
 
@@ -22,4 +37,9 @@ public class CustomerWebController {
         return "/customer/custSelect";
     }
 
+    @GetMapping("/custUpdate")
+    @ApiOperation("회원정보수정 페이지")
+    public String update() {
+        return "/customer/custUpdate";
+    }
 }
