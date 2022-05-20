e6682c191f79e01913b172b08564ad2160b15d7d
Drigter1999 <yah9600@gmail.com>
2022-05-20 12:11:12 +0900

    이상기

--- TheaterApp/src/main/java/com/example/demo/theater/normaltheaterseat/NormalTheaterSeatRepository.java
+++ TheaterApp/src/main/java/com/example/demo/theater/normaltheaterseat/NormalTheaterSeatRepository.java
@@ -1,8 +1,8 @@
-package com.example.demo.theater.normaltheaterseat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NormalTheaterSeatRepository extends JpaRepository<NormalTheaterSeat,String> {
-}
+//package com.example.demo.theater.normaltheaterseat;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface NormalTheaterSeatRepository extends JpaRepository<NormalTheaterSeat,String> {
+//}
