c5ea7cbce070571b5182fcffbde93c1bcbc3d3b7
IWannaGoMars <73753121+yeongmin7870@users.noreply.github.com>
2022-12-31 14:01:21 +0900

    수정

--- TheaterApp/src/main/java/com/example/demo/theater/exception/CustomizeResponseEntityExceptionHandler.java
+++ TheaterApp/src/main/java/com/example/demo/theater/exception/CustomizeResponseEntityExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import java.util.Date;
 
 @RestController
--- TheaterApp/src/main/java/com/example/demo/theater/exception/ExceptionResponse.java
+++ TheaterApp/src/main/java/com/example/demo/theater/exception/ExceptionResponse.java
@@ -1,14 +1,9 @@
 package com.example.demo.theater.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.Date;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ExceptionResponse {
     private Date timestamp;
     private String message;
